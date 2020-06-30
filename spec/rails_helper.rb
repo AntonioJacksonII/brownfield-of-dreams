cef7f1828e6b716859debe0cfcfa0852370c244f
Antonio Jackson <antoniojackson@gmail.com>
2020-06-29 19:55:16 -0600

    Setting up a custom driver for chrome in the rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,14 @@ end
 
 Capybara.javascript_driver = :selenium_chrome_headless
 
+Capybara.register_driver :chrome do |app|
+	options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
+
+	Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :chrome
+
 Capybara.configure do |config|
   config.default_max_wait_time = 5
 end
