208450a70605ca5a89b5028d4c0d8ff0a8a19718
Antonio Jackson <antoniojackson@gmail.com>
2020-06-30 19:00:03 -0600

    Adding test that user can see their Github repos and test for validation of github_token for the user

--- spec/features/user/user_can_view_github_repos_on_dashboard_spec.rb
+++ spec/features/user/user_can_view_github_repos_on_dashboard_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User' do
-  it 'user can see a GitHub section' do
+  it 'user can see a Github section' do
     user = create(:user)
 
     visit '/'
@@ -17,6 +17,26 @@ describe 'User' do
 
     expect(current_path).to eq(dashboard_path)
 
-    expect(page).to have_content("Github")
+    expect(page).to have_content('Github')
+  end
+
+  xit 'user can see their Github repos' do
+    user = create(:user)
+
+    visit '/'
+
+    click_on "Sign In"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in 'session[email]', with: user.email
+    fill_in 'session[password]', with: user.password
+
+    click_on 'Log In'
+
+    expect(current_path).to eq(dashboard_path)
+
+    expect(page).to have_content('Github')
+    expect(page).to have_link('Repository 1')
   end
 end
