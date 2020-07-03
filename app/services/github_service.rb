d58c5e0593d79bd30ba33a9eb32ccc23a8e342e0
Antonio Jackson <antoniojackson@gmail.com>
2020-07-03 13:13:31 -0600

    Creating GithubService and adding the followers method to get followers from the API

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,19 +1,15 @@
-class SearchResults
+class GithubService
 
-  def repos
+  def followers
     conn = Faraday.new('https://api.github.com') do |req|
       req.headers['Authorization'] = "token #{ENV['GITHUB_TOKEN']}"
     end
 
     response = conn.get('/user/repos')
     json = JSON.parse(response.body, symbolize_names: true)
-    json.take(5)
+    @repos = json.take(5)
+
+    followers_response = conn.get('/user/followers')
+    parsed_followers = JSON.parse(followers_response.body, symbolize_names: true)
   end
-
-  def followers
-    json = GithubService.new.followers
-    @followers = json.map{|followers_data| Follower.new(followers_data)}
-  end
-
-
 end
