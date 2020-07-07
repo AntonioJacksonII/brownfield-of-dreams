6d1fdec5bcee81a84408e77ccf1535584241bccc
Antonio Jackson <antoniojackson@gmail.com>
2020-07-06 19:33:58 -0600

    Removing pry from Admin PlaylistsController

--- app/controllers/admin/playlists_controller.rb
+++ app/controllers/admin/playlists_controller.rb
@@ -5,7 +5,6 @@ class Admin::PlaylistsController < ApplicationController
     tutorial = Tutorial.create(tutorial_params)
     json = YoutubeService.new.playlist_info(params[:playlist_id])
     @videos = json[:items]
-    require "pry"; binding.pry
     @videos.each do |video|
       attributes = { 'title' => video[:snippet][:title],
                      'description' => video[:snippet][:description],
