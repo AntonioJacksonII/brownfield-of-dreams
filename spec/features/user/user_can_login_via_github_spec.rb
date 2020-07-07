3e3a00fbc43f02bf8ccdbda27c5f13a95fc4e4b3
Antonio Jackson <antoniojackson@gmail.com>
2020-07-07 14:15:56 -0600

    Updating the 'user can login via Github' test to utilize OmniAuth mock auth for the OAuth handshake

--- spec/features/user/user_can_login_via_github_spec.rb
+++ spec/features/user/user_can_login_via_github_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 describe 'User' do
-  it 'user can login via Github' do
+  before :each do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      :provider => 'github',
+      :info => {:email => user.email},
+      :credentials => {:token => ENV['GITHUB_TOKEN']}
+    })
+  end
+  it 'user can login via Github' do
     visit dashboard_path
-    expect(page).to have_link('Connect to Github')
+    click_link 'Connect to Github'
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_css('.github')
+    expect(page).to have_css('.followers')
+    expect(page).to have_css('.following-group')
   end
 end
